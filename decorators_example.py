e4007aa7fb16b01f9117609fa79e5b18257b058b
efespitias <efespitias@unal.edu.co>
2025-11-26 16:43:32 +0100

    update decorators_example.py

--- decorators_example.py
+++ decorators_example.py
@@ -1,32 +1,18 @@
 # Example of decorator 
-
 import time 
 
+# Defintition of the add_time decorator
 def add_time(func):
     def myinner():
         time_initial = time.time()
-        
         func()
-        # time_initial = time.ctime(1627908313.717886)
         print(f"cooking time {(time.time()-time_initial):0.2f}")
-
     return myinner
-
 
 @add_time
 def cooking():
   print("cooking !")
   time.sleep(2)
 
-cooking()
-
-
-
-
-
-    
-
-
-
-
-
+if __name__ == "__main__":
+    cooking()
