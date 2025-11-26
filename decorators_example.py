0f43f4bb0c1db3843a91b005fe5537eddad0b56b
edgar.espitia <fbespitia@gmail.com>
2025-11-26 16:39:04 +0100

    added time functionality

--- decorators_example.py
+++ decorators_example.py
@@ -1,14 +1,22 @@
 # Example of decorator 
+
+import time 
+
 def add_time(func):
     def myinner():
-        print("time")
+        time_initial = time.time()
+        
         func()
+        # time_initial = time.ctime(1627908313.717886)
+        print(f"cooking time {(time.time()-time_initial):0.2f}")
+
     return myinner
 
 
 @add_time
 def cooking():
   print("cooking !")
+  time.sleep(2)
 
 cooking()
 
